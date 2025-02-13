ad9657ae5cba7cecca87fe8aee18531342a46b4c
Ross Angus <ross.a.angus@gmail.com>
2025-02-13 14:04:38 +0000

    feat: addEmoji streamlined default export

--- src/js/modules/add-emoji/add-emoji.js
+++ src/js/modules/add-emoji/add-emoji.js
@@ -1,7 +1,5 @@
-const addEmoji = (emoji = '😊', selector = 'body') => {
+export default addEmoji = (emoji = '😊', selector = 'body') => {
   const element = document.querySelector(selector);
   element && element.insertAdjacentHTML('afterbegin', `<big>${emoji}</big>`);
 
 };
-
-export default addEmoji;
